585d3d82c9fe9d7ca3c6b4eb2f818a10e3839caf
yulalenk <yulya.alenkova@mail.ru>
2024-04-01 20:59:01 +0300

    add basic spring boot app structure

---
+++ src/main/java/com/itmo/traveltalk/TravelTalkServerBootApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.traveltalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTalkServerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelTalkServerBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itmo/traveltalk/TravelTalkServerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.traveltalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTalkServerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
